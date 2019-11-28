2a4232be51eed86a1425bf005e8429a11b6ae8ca
lihao <lihaohao@bjca.org.cn>
2019-11-28 15:31:10 +0800

    修改

--- react-native-ywx-sign/RNReactNativeYwxSign/RNReactNativeYwxSign.h
+++ react-native-ywx-sign/RNReactNativeYwxSign/RNReactNativeYwxSign.h
@@ -1,9 +1,6 @@
 
-#if __has_include("RCTBridgeModule.h")
+
 #import "RCTBridgeModule.h"
-#else
-#import <React/RCTBridgeModule.h>
-#endif
 
 @interface RNReactNativeYwxSign : NSObject <RCTBridgeModule>
 
